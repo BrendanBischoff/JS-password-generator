c67b73e04e7ca4e1a4a1e2297d227d4a930e0746
Brendan Bischoff <brendanbischoff@gmail.com>
2021-11-16 16:36:59 -0500

    added some test variables and basic structure.

---
+++ Develop/script.js
@@ -0,0 +1,36 @@
+// Assignment code here
+
+// Generator Structure
+
+var abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var abcUpperArr = abcUpper.split("");
+var abcLower = "abcdefghijklmnopqrstuvwxyz";
+var abcLowerArr = abcLower.split("");
+var sym = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+var symArr = sym.split("");
+var num = "0123456789";
+var numArr = num.split("");
+
+// User Instructions
+
+window.onload = alert("Welcome! Please click 'Generate password' to begin!");
+
+
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# JS-password-generator
-This is a javascript project that required me to refactor the starter code to allow the user to generate a random password based on the user input criteria.
+# Password Generator Starter Code
