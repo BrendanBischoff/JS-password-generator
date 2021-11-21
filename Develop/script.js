c3742dba70a7ff825f48a2d74e21ea439f7ff565
Brendan Bischoff <brendanbischoff@gmail.com>
2021-11-21 08:44:02 -0500

    added arays, variable declations, character count confirmation.

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,29 @@
-// Assignment code here
+// Click that begins criteria questions
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Generator Structure
+// Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var abcUpperArr = abcUpper.split("");
-var abcLower = "abcdefghijklmnopqrstuvwxyz";
-var abcLowerArr = abcLower.split("");
-var sym = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
-var symArr = sym.split("");
-var num = "0123456789";
-var numArr = num.split("");
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
-// User Instructions
+// Character count confirmation prompt
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
-window.onload = alert("Welcome! Please click 'Generate password' to begin!");
+  // Loop if answer is outside requirements
+  while(confirmLength <= 7 || confirmLength >= 151) {
+      alert("Password length must be between 8-150 characters Try again");
+      var confirmLength = (prompt("How many characters would you like your password to contain?"));
+      } 
 
-
-
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+      // Repeat to user how many characters password will contain 
+      alert(`Your password will have ${confirmLength} characters`);
+    }
